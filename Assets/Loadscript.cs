615cd6b87e147d87a3c250aa6e29d7dd42db732b
Samuel Flowers <475197@chrome.escg.ac.uk>
2021-03-06 22:04:57 +0000

    First

---
+++ Assets/Loadscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loadscript : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    public void ButtonStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+}
